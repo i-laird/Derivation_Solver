aec9b869d009432ad6cdfa15aa45f94a31984f2b
Ian Laird <ian_laird@baylor.edu>
2019-06-03 13:36:46 -0600

    added shunting yard algorithm

--- src/Parser.java
+++ src/Parser.java
@@ -1,13 +1,9 @@
-import Enums.AbstractMath;
-import Enums.Function;
-import Enums.Num;
-import Enums.Operator;
+import Enums.*;
+import Terms.Term;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Parser {
@@ -17,12 +13,44 @@ public class Parser {
         String [] parts = line.split("\\s+");
 
         // first run the shunting yard algorithm
-        List<AbstractMath> mappedParts = Arrays.stream(parts).map(Operator::getOp).collect(Collectors.toList());
+        List<AbstractMath> mappedParts = Arrays.stream(parts).map(Parser::getMappedPart).collect(Collectors.toList()),
+                            outputParts = new LinkedList<>(); //these are those that would be written to console
+        Stack<AbstractMath>  stack = new Stack<>();
+        for(AbstractMath am: mappedParts){
+            if(am.getClass() == Num.class){
+                outputParts.add(am);
+            }
+            if(am.getClass() == Function.class) {
+                stack.push(am);
+            }
+            if(am.getClass() == Paren.class){\
+                if(am == Paren.LEFT_PAREN){
+                    stack.push(am);
+                }
+                else{
+                    while(stack.peek() != Paren.LEFT_PAREN){
+                        outputParts.add(stack.pop());
+                    }
+                }
+                continue;
+            }
+            //if not a functin or a number it must be an operator
+            while(!stack.empty() && ((Operator)stack.peek()).precedence >= ((Operator)am).precedence){
+                outputParts.add(stack.pop());
+            }
+            stack.push(am);
+        }
 
         // then analyze the expression now that it is reverse polish notation
     }
 
-    public AbstractMath getMappedPart(String s){
+    public static AbstractMath getMappedPart(String s){
+        if(s.matches("\\(")){
+            return Paren.LEFT_PAREN;
+        }
+        if(s.matches("\\)")){
+            return Paren.RIGHT_PAREN;
+        }
         AbstractMath returnThing = null;
         //see if it is an integeer
         if (s.matches("[+-]?\\d+")){
