c87499ebe8bc9b70bef579c3115381b636b0bc31
i-laird <ian.c.laird@hotmail.com>
2020-02-18 22:25:28 -0600

    adding comments

--- src/main/java/DerivationSolver/DTO/Response.java
+++ src/main/java/DerivationSolver/DTO/Response.java
@@ -1,7 +1,15 @@
 package DerivationSolver.DTO;
 
+/**
+ * Response DTO
+ * @author Ian Laird
+ */
 public class Response {
+
+    // the calculated antiderivative
     public String antiderivative;
+
+    // the antiderivative value when evaluated
     public Double result;
 
     public Response(String antiderivative, Double result) {
--- src/main/java/DerivationSolver/util/StringToStream.java
+++ src/main/java/DerivationSolver/util/StringToStream.java
@@ -4,8 +4,16 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * @author Ian Laird
+ */
 public class StringToStream {
 
+    /**
+     * converts a string to an input stream
+     * @param str the string to convert
+     * @return an input stream whose contents are the string
+     */
     public static InputStream convertStringToStream(String str){
         return new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
     }
