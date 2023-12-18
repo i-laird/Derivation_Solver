cd2dba628dccc946c13dd3ab303e10a5802f5259
Ian Laird <ianclaird@gmail.com>
2023-12-17 17:06:35 -0800

    Linting Fix + SecretKey (#27)

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -4,7 +4,6 @@ import calculator.DTO.DerivativeRequest;
 import calculator.DTO.DerivativeResponse;
 import calculator.service.CalculatorService;
 import java.time.LocalTime;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
