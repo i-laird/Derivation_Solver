28bf6e126139c5e42aef5e8e0435bef2a31c2ef8
Ian Laird <ian.c.laird@hotmail.com>
2026-02-26 22:22:00 -0800

    Fix bug

--- src/main/java/calculator/security/WebSecurityConfig.java
+++ src/main/java/calculator/security/WebSecurityConfig.java
@@ -14,7 +14,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-:@Configuration
+
+@Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
