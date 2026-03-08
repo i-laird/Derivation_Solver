9c72285cb41237c1f10c36436a1da34e0330d118
Ian Laird <secure@ilaird.com>
2026-03-07 20:59:26 -0800

    Allow frontend connection (#44)
    * Allow frontend connection
    
    * Fixed junit

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -7,7 +7,7 @@ import com.google.common.collect.ImmutableList;
 import jakarta.validation.Valid;
 import java.time.LocalTime;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +29,7 @@ public final class CalculatorController {
    * @param request The request.
    * @return the anti-derivative and result at a specific point.
    */
-  @GetMapping(value = "/derivative")
+  @PostMapping(value = "/derivative")
   @ResponseStatus(HttpStatus.OK)
   public DerivativeResponse generateDerivative(@Valid @RequestBody DerivativeRequest request) {
     return calculatorServiceImpl.evaluateDerivative(
@@ -42,7 +42,7 @@ public final class CalculatorController {
    * @param request the request.
    * @return The evaluation of a mathametical expression at specific points.
    */
-  @GetMapping(value = "/expression")
+  @PostMapping(value = "/expression")
   @ResponseStatus(HttpStatus.OK)
   public double evaluateExpression(@Valid @RequestBody DerivativeRequest request) {
     return calculatorServiceImpl.evaluateExpression(
--- src/test/java/calculator/controller/CalculatorControllerTest.java
+++ src/test/java/calculator/controller/CalculatorControllerTest.java
@@ -3,7 +3,7 @@ package calculator.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import calculator.DTO.DerivativeRequest;
@@ -56,7 +56,7 @@ class CalculatorControllerTest {
     DerivativeRequest request = new DerivativeRequest("x", List.of(1));
     mockMvc
         .perform(
-            get("/derivative")
+            post("/derivative")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isOk());
@@ -67,7 +67,7 @@ class CalculatorControllerTest {
     DerivativeRequest request = new DerivativeRequest("", List.of());
     mockMvc
         .perform(
-            get("/derivative")
+            post("/derivative")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isBadRequest());
